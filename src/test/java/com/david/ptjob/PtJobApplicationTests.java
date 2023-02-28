35ead18b8a64185d933e420d0f3d40608d1b3c48
David <hardlearner9229@gmail.com>
2023-02-28 14:33:59 +0900

    chore: 스프링부트 웹 애플리케이션 초기 파일 추가

---
+++ src/test/java/com/david/ptjob/PtJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.ptjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
