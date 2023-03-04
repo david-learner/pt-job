2faea6cf2d924abba8a0337f59395f2ca983489e
David <hardlearner9229@gmail.com>
2023-03-04 09:56:35 +0900

    feat: 아이템 만료 상태 추가

--- src/main/java/com/david/ptjob/item/domain/Item.java
+++ src/main/java/com/david/ptjob/item/domain/Item.java
@@ -1,16 +1,40 @@
 package com.david.ptjob.item.domain;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
+@Entity
+@Getter
 @AllArgsConstructor(staticName = "of")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Item {
 
-    private final LocalDateTime createdAt;
-    private final Category category;
-    private final String name;
-    private final Integer price;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Transient
+    private boolean expired = false;
+
+    public void updateExpirationStatus() {
+        expired = ItemExpirationValidator.isExpiredFrom(LocalDateTime.now(), createdAt);
+    }
 }
