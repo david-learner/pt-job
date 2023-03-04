6d95ec2d806691a36608e0059c6b7a133e8b79a5
David <hardlearner9229@gmail.com>
2023-03-04 11:24:37 +0900

    chore: retry 설정 추가

--- src/main/java/com/david/ptjob/PtJobApplication.java
+++ src/main/java/com/david/ptjob/PtJobApplication.java
@@ -5,8 +5,10 @@ import com.david.ptjob.infra.item.vegetable.VegetableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 @EnableConfigurationProperties(value = {FruitProperties.class, VegetableProperties.class})
 public class PtJobApplication {
 
